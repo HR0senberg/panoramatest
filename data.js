79e08642c153b4daf7b96b7eddfdf6551cad9c9f
HR0senberg <74983814+HR0senberg@users.noreply.github.com>
2024-02-04 00:48:19 +0300

    1

---
+++ data.js
@@ -0,0 +1,100 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6324914634371392,
+          "pitch": 0.22407218814922203,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Ракета",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.0938865996823122,
+        "pitch": -0.1790972924879135,
+        "fov": 1.3608936558192368
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9602506651604763,
+          "pitch": 0.20697482887923613,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0808679617990098,
+          "pitch": -0.024235185340076626,
+          "title": "<p class=\"MsoNormal\" style=\"font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 300; font-size: 13px; font-family: Helvetica, Arial, sans-serif; background-color: rgba(58, 68, 84, 0.8);\"><b><span style=\"font-size: 12pt; line-height: 17.12px; font-family: &quot;Times New Roman&quot;, serif;\"><a href=\"https://youtu.be/B0ntNkEMYnU?t=258\">Макет-памятник<o:p></o:p></a></span></b></p><p class=\"MsoNormal\" style=\"font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 300; font-size: 13px; font-family: Helvetica, Arial, sans-serif; background-color: rgba(58, 68, 84, 0.8);\"><span class=\"MsoHyperlink\"><b><span style=\"font-size: 12pt; line-height: 17.12px; font-family: &quot;Times New Roman&quot;, serif;\"><a href=\"https://youtu.be/B0ntNkEMYnU?t=258\">противоракете<o:p></o:p></a></span></b></span></p><p class=\"MsoNormal\" style=\"font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 300; font-size: 13px; font-family: Helvetica, Arial, sans-serif; background-color: rgba(58, 68, 84, 0.8);\"><span class=\"MsoHyperlink\"><b><span style=\"font-size: 12pt; line-height: 17.12px; font-family: &quot;Times New Roman&quot;, serif;\"><a href=\"https://youtu.be/B0ntNkEMYnU?t=258\">B-1000 (2016)</a></span></b></span></p>",
+          "text": "<p class=\"MsoNormal\"><b><span style=\"font-size:12.0pt;line-height:107%;font-family:&quot;Times New Roman&quot;,serif\"><a href=\"https://hr0senberg.github.io/1430test/exhibits.htm\">Макет-памятник<o:p></o:p></a></span></b></p><p class=\"MsoNormal\"><span class=\"MsoHyperlink\"><b><span style=\"font-size:12.0pt;line-height:107%;font-family:&quot;Times New Roman&quot;,serif\"><a href=\"https://hr0senberg.github.io/1430test/exhibits.htm\">противоракете<o:p></o:p></a></span></b></span></p><p class=\"MsoNormal\">\n\n\n\n</p><p class=\"MsoNormal\"><span class=\"MsoHyperlink\"><b><span style=\"font-size:12.0pt;line-height:107%;font-family:&quot;Times New Roman&quot;,serif\"><a href=\"https://hr0senberg.github.io/1430test/exhibits.htm\">B-1000 (2016)</a></span></b></span><b><span style=\"font-size:12.0pt;line-height:\n107%;font-family:&quot;Times New Roman&quot;,serif\"><o:p></o:p></span></b></p>"
+        },
+        {
+          "yaw": -2.418491123371542,
+          "pitch": 0.25597871590783505,
+          "title": "Title",
+          "text": "<img src=\"https://hr0senberg.github.io/1430test/index_htm_files/92.jpg\">"
+        }
+      ]
+    }
+  ],
+  "name": "3Д тур",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
